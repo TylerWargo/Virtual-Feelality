08dcf1dd21749557cead364f8838412c93e336b2
Tyler Wargo <tylerwargo@outlook.com>
2018-05-01 20:32:54 -0400

    Added promotional content and source files / code.
    'Virtual Feelality' Unity game files added w/ meta files. Includes Leap Motion SDK, complying with licenses. Promotional image also added - used for poster IRL.

---
+++ Virtual Feelality/Assets/Virtual Feelality/Scripts/FingerCollision.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//(2018) Made by Tyler Wargo
+
+public class FingerCollision : MonoBehaviour 
+{
+	//Acces Serial Script
+	public SerialControl serialScript;
+	//Colliding Finger GO
+	public GameObject fingerGO;
+	//Current Finger
+	public string currentFinger;
+
+	public void Start()
+	{
+		//Sets Current Finger Touching = Specific Finger GO Tag
+		currentFinger = fingerGO.tag;
+	}
+
+	//Check for Collision / Touch
+	public void OnCollisionEnter(Collision col)
+	{
+		//Check If Colliding With 'Touchable' Object
+		if (col.gameObject.tag == "Touchable") 
+		{
+			//Thumb On Collision
+			if (currentFinger == "Thumb Tip Right") 
+			{
+				//THUMB ON
+				serialScript.serial.Write ("A");
+				Debug.Log ("Thumb Touch");
+			}
+			//Index On Collision
+			else if (currentFinger == "Index Tip Right") 
+			{
+				//INDEX ON
+				serialScript.serial.Write ("C");
+				Debug.Log ("Index Touch");
+			}
+			//Middle On Collision
+			else if(currentFinger == "Middle Tip Right")
+			{
+				//MIDDLE ON
+				serialScript.serial.Write ("E");
+				Debug.Log ("Middle Touch");
+			}
+			//Ring On Collision
+			else if(currentFinger == "Ring Tip Right")
+			{
+				//RING ON
+				serialScript.serial.Write ("G");
+				Debug.Log ("Ring Touch");
+			}
+			//Pinky On Collision
+			else if(currentFinger == "Pinky Tip Right")
+			{
+				//PINKY ON
+				serialScript.serial.Write ("I");
+				Debug.Log ("Pinky Touch");
+			}
+		}
+	}
+
+	//Check for exiting Collision / Touch
+	public void OnCollisionExit(Collision col)
+	{
+		//Check If NOT Colliding (Has Exited) With 'Touchable' Object
+		if (col.gameObject.tag == "Touchable") 
+		{
+			//Thumb Off Collision
+			if (currentFinger == "Thumb Tip Right") 
+			{
+				//THUMB OFF
+				serialScript.serial.Write ("B");
+			}
+			//Index Off Collision
+			else if (currentFinger == "Index Tip Right") 
+			{
+				//INDEX OFF
+				serialScript.serial.Write ("D");
+			}
+			//Middle Off Collision
+			else if (currentFinger == "Middle Tip Right") 
+			{
+				//MIDDLE OFF
+				serialScript.serial.Write("F");
+			}
+			//Ring off Collision
+			else if(currentFinger == "Ring Tip Right")
+			{
+				//RING OFF
+				serialScript.serial.Write ("H");
+			}
+			//Pinky off Collision
+			else if(currentFinger == "Pinky Tip Right")
+			{
+				//PINKY OFF
+				serialScript.serial.Write ("J");
+			}
+		}
+	}
+}
---
+++ Virtual Feelality/Assets/Virtual Feelality/Scripts/SerialControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+//(2018) Made by Tyler Wargo
+
+public class SerialControl : MonoBehaviour 
+{
+	//Set Serial Port Data Stream
+	public SerialPort serial = new SerialPort("COM7", 115200);
+
+	public void Update()
+	{
+		//Check If Serial Data Stream Is Closed
+		if (!serial.IsOpen)
+			//Open Data Stream If Closed
+			serial.Open ();
+	}
+}
